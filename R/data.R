ca0896d407cf20a5d6648b754199c2b4b16cc153
vjcitn <stvjc@channing.harvard.edu>
2023-07-16 16:14:55 +0000

    scans in

--- R/data.R
+++ R/data.R
@@ -2,3 +2,13 @@
 #' @docType data
 #' @format data.frame
 "simplecore"
+
+#' scan on 16 july
+#' @docType data
+#' @format artifset_scan instance
+"scan_demo_3.17"
+
+#' dev scan on 16 july
+#' @docType data
+#' @format artifset_scan instance
+"scan_demo_3.18"
