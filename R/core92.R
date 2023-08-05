d2ba228b90ad867fe1a97ec0f92658f77603ddd4
vjcitn <stvjc@channing.harvard.edu>
2023-08-05 12:50:43 -0400

    adding core92

---
+++ R/core92.R
@@ -0,0 +1,22 @@
+#' HPages collection of testing packages
+#' @export
+core92 = function() c("BiocVersion", "BiocStyle", "BiocGenerics", "DynDoc", "widgetTools", 
+"tkWidgets", "Biobase", "zlibbioc", "BiocParallel", "S4Vectors", 
+"IRanges", "GenomeInfoDb", "AnnotationDbi", "AnnotationForge", 
+"OrganismDbi", "XVector", "GenomicRanges", "KEGGgraph", "SPIA", 
+"Biostrings", "MatrixGenerics", "DelayedArray", "sparseMatrixStats", 
+"DelayedMatrixStats", "annotate", "genefilter", "ROC", "SummarizedExperiment", 
+"SingleCellExperiment", "Rhtslib", "Rsamtools", "GenomicAlignments", 
+"rtracklayer", "microRNA", "BSgenome", "BiocFileCache", "interactiveDisplayBase", 
+"AnnotationHub", "ExperimentHub", "gdsfmt", "biomaRt", "GenomicFeatures", 
+"ShortRead", "VariantAnnotation", "snpStats", "preprocessCore", 
+"affyio", "affy", "multtest", "impute", "Rhdf5lib", "rhdf5", 
+"HDF5Array", "beachmat", "BiocNeighbors", "AnnotationFilter", 
+"limma", "vsn", "illuminaio", "edgeR", "Rsubread", "geneplotter", 
+"DESeq2", "tximport", "tximeta", "apeglm", "qvalue", "fishpond", 
+"DEXSeq", "csaw", "graph", "Rgraphviz", "RBGL", "ProtGenerics", 
+"ensembldb", "biovizBase", "ggbio", "Gviz", "mzID", "mzR", "RProtoBufLib", 
+"cytolib", "flowCore", "basilisk.utils", "basilisk", "rBiopaxParser", 
+"pcaMethods", "MSnbase", "affxparser", "MsCoreUtils", "biocViews", 
+"BiocCheck")
+
